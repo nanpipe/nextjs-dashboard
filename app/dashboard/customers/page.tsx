599034d3a3a647630dced030ef0a54f47c98fae2
Pipe Solarte <nanpipe00@gmail.com>
2024-03-03 20:19:30 -0500

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+import { lusitana } from '@/app/ui/fonts';
+
+
+export default function Customers() {
+    return <p className={`${lusitana.className} center p-10 w-20`}>Customer Page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,6 @@
+import { lusitana } from '@/app/ui/fonts';
+
+
+export default function Invoices() {
+    return <p className={`${lusitana.className}`}>invoice Page</p>;
+}
